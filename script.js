9e1bf6b6037c2e425e244a208b4586aae6bb6de7
jamescorriveau <137570538+jamescorriveau@users.noreply.github.com>
2024-08-02 17:40:08 -0400

    fixed contact page jumping bug

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-const menuLinks = document.querySelectorAll(".menu-links a");
-menuLinks.forEach((link) => {
-  link.addEventListener("click", (event) => {
-    console.log(`${event.target.innerText} link clicked`);
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.remove("open");
-    icon.classList.remove("open");
+document.addEventListener("DOMContentLoaded", () => {
+  const menuLinks = document.querySelectorAll(".menu-links a");
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      console.log(`${event.target.innerText} link clicked`);
+      const menu = document.querySelector(".menu-links");
+      const icon = document.querySelector(".hamburger-icon");
+      menu.classList.remove("open");
+      icon.classList.remove("open");
+    });
   });
 });
