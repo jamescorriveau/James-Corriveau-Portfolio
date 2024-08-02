cc453900f2c36fea2128f0d62e0a5dd14e79a058
jamescorriveau <137570538+jamescorriveau@users.noreply.github.com>
2024-08-02 15:37:16 -0400

    fixed hamburger-nav bugs

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const menuLinks = document.querySelectorAll(".menu-links a");
+menuLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    console.log(`${event.target.innerText} link clicked`);
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.remove("open");
+    icon.classList.remove("open");
+  });
+});
